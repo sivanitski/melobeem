7fbdf188333bddb93fc21fbe727ff841a9021243
Nikolay Kuzmenko <kuzmenkojob@gmail.com>
2021-01-14 16:02:59 +0300

    add user and devise token auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'application#index'
+
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
+  end
 end
