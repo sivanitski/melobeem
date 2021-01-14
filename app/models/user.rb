39948b2f7543283f472be1be62a6cd9cd8de6141
Nikolay Kuzmenko <kuzmenkojob@gmail.com>
2021-01-14 17:02:17 +0300

    add specs for authentication. specs for user model. add permitted parameters into registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
   include DeviseTokenAuth::Concerns::User
+
+  validates :name, :provider, presence: true
 end
