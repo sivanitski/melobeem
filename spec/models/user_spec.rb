abdadc89cb24317ce74680caf091a227172d549b
glebson1988 <glebson1988@yandex.ru>
2021-01-15 19:02:33 +0300

    fix rubocop and schema

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   it { is_expected.to have_many(:entries).dependent(:destroy) }
   it { is_expected.to have_many(:votes).dependent(:destroy) }
 
