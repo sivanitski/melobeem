1bc6aa4d5168f29b492644a12d80a0cf7676e973
Nikolay Kuzmenko <kuzmenkojob@gmail.com>
2021-01-27 14:17:47 +0300

    use environment variables for active storage host

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,9 +1,5 @@
 Rails.application.reloader.to_prepare do
   # TODO: modify settings for any other environment
 
-  ActiveStorage::Current.host = if Rails.env.development?
-                                  'http://localhost:3000'
-                                else
-                                  'https://cdn.domain.com'
-                                end
+  ActiveStorage::Current.host = ENV.fetch('ACTIVE_STORAGE_HOST', 'http://localhost:3000')
 end
