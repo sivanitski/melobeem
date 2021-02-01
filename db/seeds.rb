865f0167315939d9f751e41e39aad332cd0f7cfa
glebson1988 <glebson1988@yandex.ru>
2021-02-01 18:49:26 +0300

    adding seeds without pics

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'ffaker'
+
+competition = Competition.create!(title: 'Kiddy')
+
+# generate 20 users
+(1..20).each do |id|
+  User.create!(
+    id: id,
+    name: FFaker::Name.name,
+    email: FFaker::Internet.email,
+    password: "password",
+    password_confirmation: "password",
+  )
+end
+
+# generate 20 entries
+(1..20).each do |id|
+  Entry.create!(
+    id: id,
+    gender: FFaker::Gender.binary,
+    user_id: id,
+    competition_id: competition.id
+  )
+end
+
+#generate 2000 votes
+(1..2000).each do |id|
+  Vote.create!(
+    id: id,
+    entry_id: rand(1..20),
+    user_id: rand(1..20)
+  )
+end
