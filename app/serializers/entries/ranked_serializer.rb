223ea27123f8bf536a98062305d10430a3887011
Nikolay Kuzmenko <kuzmenkojob@gmail.com>
2021-02-26 14:34:18 +0300

    stub level attribute in serializer

--- app/serializers/entries/ranked_serializer.rb
+++ app/serializers/entries/ranked_serializer.rb
@@ -1,5 +1,9 @@
 module Entries
   class RankedSerializer < Entries::BaseSerializer
-    attributes :id, :gender, :name, :image_url, :total_votes, :rank, :username
+    attributes :id, :gender, :name, :image_url, :total_votes, :rank, :username, :level
+
+    def level
+      # stub
+    end
   end
 end
