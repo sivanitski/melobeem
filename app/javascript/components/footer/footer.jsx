3603469c51cb2b9a3bff90e93efc7dfdc8f10737
Maria Frei <itimasha@gmail.com>
2021-03-11 16:35:52 +0300

    User entry in footer (#110)
    * initial
    
    * show current entry in footer

--- app/javascript/components/footer/footer.jsx
+++ app/javascript/components/footer/footer.jsx
@@ -1,8 +1,11 @@
 import "./style.less";
 
-import React from "react";
+import { useRequest } from "ahooks";
+import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 
+import { createAPI } from "../../api";
+import UserContext from "../../helpers/user-context";
 import IconButton from "../../images/button.svg";
 import IconLeaderboard from "../../images/icon-leaderboard.svg";
 import IconLevels from "../../images/icon-levels.svg";
@@ -10,6 +13,40 @@ import IconProfile from "../../images/icon-profile.svg";
 import IconSpinner from "../../images/icon-spinner.svg";
 
 const Footer = () => {
+  const { user } = useContext(UserContext);
+  const api = createAPI();
+  const getCurrentBaby = () => {
+    return api.get("/entries/current");
+  };
+
+  const { data, run, loading } = useRequest(getCurrentBaby, {
+    formatResult: (res) => res.data.entry,
+  });
+
+  useEffect(() => {
+    run();
+  }, [user]);
+
+  const renderCentralButton = (baby) => {
+    if (loading) {
+      return <div className="footer__button"></div>;
+    }
+
+    if (baby) {
+      return (
+        <Link to={`/entry/${baby.id}`} className="footer__button">
+          <img src={baby.imageUrl} />
+        </Link>
+      );
+    }
+
+    return (
+      <Link to="/sign-up" className="footer__button">
+        <IconButton />
+      </Link>
+    );
+  };
+
   return (
     <div className="footer">
       <Link to="/" className="footer__item">
@@ -20,21 +57,15 @@ const Footer = () => {
         <IconSpinner />
         <div className="footer__item__title">Spinner</div>
       </a>
-      <a className="footer__button">
-        <Link to="/sign-up">
-          <IconButton />
-        </Link>
-      </a>
+      {renderCentralButton(data)}
       <a className="footer__item">
         <IconLevels />
         <div className="footer__item__title">Levels</div>
       </a>
-      <a className="footer__item">
-        <Link to="/profile" className="footer__item">
-          <IconProfile />
-          <div className="footer__item__title">Profile</div>
-        </Link>
-      </a>
+      <Link to="/profile" className="footer__item">
+        <IconProfile />
+        <div className="footer__item__title">Profile</div>
+      </Link>
     </div>
   );
 };
