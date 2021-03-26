fd4ca66d112806b8c8a83411bfb1d7b72d65f6ae
Anton Savicky <SavickyAnt@users.noreply.github.com>
2021-03-26 07:03:22 +0200

    wip test fb.login

--- app/javascript/components/facebook-login/facebook-login.jsx
+++ app/javascript/components/facebook-login/facebook-login.jsx
@@ -5,6 +5,7 @@ import { FacebookContext, LoginButton } from "react-facebook";
 import { createAPI, createFbAPI } from "../../api";
 import ChildContext from "../../helpers/child-context";
 import UserContext from "../../helpers/user-context";
+import axios from 'axios'
 
 const FacebookLogin = ({ title, classes, handleLogin }) => {
   const { setUser } = useContext(UserContext);
@@ -16,20 +17,39 @@ const FacebookLogin = ({ title, classes, handleLogin }) => {
       setAppId(facebookContext.api.options.appId);
     }
   }, [facebookContext.isReady]);
-  const apiFb = createFbAPI();
+  // const apiFb = createFbAPI();
   const api = createAPI();
 
-  const handleResponse = async (data) => {
+  const handleResponse = async (evn) => {
+    // console.log(evn)
+    evn.preventDefault();
     // { cookie: true } for FB.init does not work. We'll have to set the required cookie manually
-    document.cookie = `fbsr_${appId}=${data.tokenDetail.signedRequest}`;
 
-    const res = await apiFb.get(``);
-    const child = await api.get(`/entries/current`);
-    setCurrentChild(child.data.entry);
-    setUser(res.data.user);
-    if (handleLogin) {
-      handleLogin();
-    }
+    FB.login(function(res) {
+      if (res.authResponse) {
+        console.log('Welcome!  Fetching your information.... ');
+        document.cookie = `fbsr_${appId}=${res.authResponse.signedRequest}`;
+        console.log(res)
+        const resAuth = axios.get(`/users/auth/facebook/callback`).then(function(resAuth) {
+          console.log('data fetched')
+          console.log(resAuth)
+          setUser(resAuth.data.user);
+          if (handleLogin) {
+            handleLogin();
+          }
+        });
+
+        // const child = await api.get(`/entries/current`);
+        // console.log(child)
+        // setCurrentChild(child.data.entry);
+
+        // FB.api('/me', function(response) {
+        //   console.log('Good to see you, ' + response.name + '.');
+        // });
+      } else {
+        console.log('User cancelled login or did not fully authorize.');
+      }
+    },{scope: 'email'});
   };
 
   const handleError = (error) => {
@@ -37,9 +57,9 @@ const FacebookLogin = ({ title, classes, handleLogin }) => {
   };
 
   return (
-    <LoginButton onCompleted={handleResponse} onError={handleError}>
+    <a onClick={handleResponse}>
       <span className={`button button--facebook ${classes}`}>{title}</span>
-    </LoginButton>
+    </a>
   );
 };
 
