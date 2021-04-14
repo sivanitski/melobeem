8f7967d6a1b2fc5ce2fbfa7d93248c87560f702e
Nikolay Kuzmenko <kuzmenkojob@gmail.com>
2021-04-14 13:40:38 +0300

    add current baby name for user (#166)

--- app/serializers/users/show_serializer.rb
+++ app/serializers/users/show_serializer.rb
@@ -1,5 +1,11 @@
 module Users
   class ShowSerializer < Users::BaseSerializer
-    attributes :id, :name, :avatar_url
+    attributes :id, :name, :avatar_url, :current_baby_name
+
+    def current_baby_name
+      object.entries.where(competition: Competition.current!).name
+    rescue ActiveRecord::RecordNotFound
+      nil
+    end
   end
 end
