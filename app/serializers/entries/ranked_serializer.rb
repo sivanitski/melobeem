38d8c5140f563b76a5854569e31023dd35f2ebe1
Nikolay Kuzmenko <kuzmenkojob@gmail.com>
2021-04-28 17:36:41 +0300

    add user id into entry show (#191)

--- app/serializers/entries/ranked_serializer.rb
+++ app/serializers/entries/ranked_serializer.rb
@@ -1,5 +1,5 @@
 module Entries
   class RankedSerializer < Entries::BaseSerializer
-    attributes :id, :name, :image_url, :total_votes, :rank, :username, :level
+    attributes :id, :name, :image_url, :total_votes, :rank, :username, :level, :user_id
   end
 end
