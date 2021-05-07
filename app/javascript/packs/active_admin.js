1aa431ffe0e5f8fd0d56e5e6727dca28ef6b50be
Nikolay Kuzmenko <kuzmenkojob@gmail.com>
2021-05-07 12:51:13 +0300

    add active admin into webpack (#222)

---
+++ app/javascript/packs/active_admin.js
@@ -0,0 +1,4 @@
+// Load Active Admin's styles into Webpacker,
+// see `active_admin.scss` for customization.
+import "../stylesheets/active_admin.scss";
+import "@activeadmin/activeadmin";
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const { environment } = require("@rails/webpacker");
+const jquery = require("./plugins/jquery");
 
 const { less, svg, svgInLess } = require("./loaders");
 
@@ -9,6 +10,7 @@ environment.loaders.append("less", less);
 environment.loaders.append("svg", svg);
 environment.loaders.append("svgInLess", svgInLess);
 
+environment.plugins.prepend("jquery", jquery);
 module.exports = environment;
 
 const nodeModulesLoader = environment.loaders.get("nodeModules");
