123b159c2ce80f218cd7011b04231f00d1771e3b
Nikolay Kuzmenko <kuzmenkojob@gmail.com>
2021-06-30 15:50:41 +0300

    add chartckik into active admin to show charts (#312)

--- app/javascript/packs/active_admin.js
+++ app/javascript/packs/active_admin.js
@@ -2,3 +2,5 @@
 // see `active_admin.scss` for customization.
 import "../stylesheets/active_admin.scss";
 import "@activeadmin/activeadmin";
+// eslint-disable-next-line import/no-unresolved,import/extensions
+import "chartkick/chart.js";
