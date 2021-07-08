5e348cdaac972a84e9e696379ee4bba931e57b7c
Nikolay Kuzmenko <kuzmenkojob@gmail.com>
2021-07-08 15:24:49 +0300

    Fix docker (#328)
    * revert
    
    * try another variant
    
    * try to rescue issue

--- db/migrate/20210708103235_change_prizes_c_heck_c_onstrant.rb
+++ db/migrate/20210708103235_change_prizes_c_heck_c_onstrant.rb
@@ -2,6 +2,8 @@ class ChangePrizesCHeckCOnstrant < ActiveRecord::Migration[6.1]
   def change
     begin
       remove_check_constraint :prizes, "value = ANY (ARRAY[1, 5, 10, 20, 30])"
+    rescue
+      p 'have no constraint'
     ensure
       add_check_constraint :prizes, 'value IN (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30)'
     end
