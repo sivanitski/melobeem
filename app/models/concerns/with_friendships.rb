a58416348bd9816056661ae5301b9ed5491f67ab
tymkiv <tymkiv.vr@gmail.com>
2021-09-13 11:35:32 +0300

    changed with_friendships file

--- app/models/concerns/with_friendships.rb
+++ app/models/concerns/with_friendships.rb
@@ -1,20 +1,21 @@
 module WithFriendships
   extend ActiveSupport::Concern
-
   included do
     has_many :friendships, inverse_of: :user, dependent: :destroy
     has_many :friends,
              -> { select(select_scope) },
-             through: :friendships
+             through: :friendships,
+             dependent: :destroy
     has_many :internal_friends,
              -> { where(friendships: { source_type: 'internal' }).select(select_scope) },
              through: :friendships,
-             source: :friend
+             source: :friend,
+             dependent: :destroy
     has_many :external_friends,
              -> { where(friendships: { source_type: 'external' }).select(select_scope) },
              through: :friendships,
-             source: :friend
-
+             source: :friend,
+             dependent: :destroy
     def self.select_scope
       'users.*, friendships.source_type AS source_type, friendships.invitation_prize AS invitation_prize'
     end
