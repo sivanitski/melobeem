9dd9cbe5d28806afe083c10fb5387688342f6001
Nikolay Kuzmenko <kuzmenkojob@gmail.com>
2021-10-11 18:36:37 +0300

    add dashboard data (#426)

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,12 +2,20 @@ ActiveAdmin.register_page 'Dashboard' do # rubocop:disable Metrics/BlockLength
   menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
 
   content title: proc { I18n.t('active_admin.dashboard') } do # rubocop:disable Metrics/BlockLength
+    competition = Competition.current!
+
     columns do
       column do
         div class: 'votes-last-month' do
-          h3 'Votes: Last Month'
-          votes = Vote.select('created_at::date as created_at_day, sum(value) as vote_value')
-                      .where('created_at::date >= ?::date', Time.current - 1.month)
+          h3 'Votes: Current competition'
+
+          total_votes = Vote.joins(:entry).where(entries: { competition: competition }).count
+
+          h4 "Total votes: #{total_votes}"
+
+          votes = Vote.joins(:entry)
+                      .where(entries: { competition: competition })
+                      .select('votes.created_at::date as created_at_day, sum(votes.value) as vote_value')
                       .group('created_at_day')
                       .map { |v| [v.created_at_day, v.vote_value] }
 
@@ -17,8 +25,13 @@ ActiveAdmin.register_page 'Dashboard' do # rubocop:disable Metrics/BlockLength
 
       column do
         div class: 'users-last-month' do
-          h3 'Users: Last Month'
-          users = User.where('created_at::date >= ?::date', Time.current - 1.month)
+          h3 'Users: Current competition'
+
+          total_users = User.where('created_at::date >= ?::date', competition.starts_at).count
+
+          h4 "Total users: #{total_users}"
+
+          users = User.where('created_at::date >= ?::date', competition.starts_at)
                       .group('created_at::date')
                       .count
                       .map { |u| [u.first, u.last] }
@@ -31,9 +44,14 @@ ActiveAdmin.register_page 'Dashboard' do # rubocop:disable Metrics/BlockLength
     columns do
       column do
         div class: 'purchases-last-month' do
-          h3 'Purchases: Last Month'
+          h3 'Purchases: Current competition'
+
+          total_transactions = PurchaseTransaction.where(status: :done).where(competition: competition).count
+
+          h4 "Total transactions: #{total_transactions}"
+
           transactions = PurchaseTransaction.where(status: :done)
-                                            .where('created_at::date >= ?::date', Time.current - 1.month)
+                                            .where(competition: competition)
                                             .group('created_at::date')
                                             .count
                                             .map { |u| [u.first, u.last] }
@@ -44,8 +62,13 @@ ActiveAdmin.register_page 'Dashboard' do # rubocop:disable Metrics/BlockLength
 
       column do
         div class: 'purchases-last-month' do
-          h3 'Entries: Last Month'
-          entries = Entry.where('created_at::date >= ?::date', Time.current - 1.month)
+          h3 'Entries: Current competition'
+
+          total_entries = Entry.where(competition: competition).count
+
+          h4 "Total entries: #{total_entries}"
+
+          entries = Entry.where(competition: competition)
                          .group('created_at::date')
                          .count
                          .map { |u| [u.first, u.last] }
